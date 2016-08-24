9c2e67e01c4a97e3adab0624054d5505664a128f
Elias Basler <e.e.basler@gmail.com>
2016-08-24 13:10:00 -0400

    bored

---
+++ prime.py
@@ -0,0 +1,16 @@
+import math
+place = 2
+number = 3
+square = 0
+loop = 0
+while(place <= 100):
+    square = math.sqrt(number)
+    loop = 2
+    while(loop<=square):
+        if(number%loop==0):
+            break
+        loop += 1
+    else:
+        print str(place) + ": " + str(number)
+        place +=1
+    number += 2
