71049d4569a3ef572c336a35f6babf1f7984b5cf
Elias Basler <e.e.basler@gmail.com>
2016-10-05 13:55:32 -0400

    more fine-tuning

--- Robticscv.py
+++ Robticscv.py
@@ -13,10 +13,13 @@ for s in os.listdir("files"):
     #im = cv.bitwise_not(im)
 
     im2 = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
+    #cv.imshow('gray', im2)
 
-    th, im3 = cv.threshold(im2, 100, 255, cv.THRESH_BINARY);
+    th, im3 = cv.threshold(im2, 80, 255, cv.THRESH_BINARY);
+    #cv.imshow('threshold', im3)
 
     contours, hierarchy = cv.findContours(im3,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+
 
     lan = 0
     thing = 0
@@ -25,14 +28,15 @@ for s in os.listdir("files"):
             lan = (((cv.contourArea(cv.convexHull(c))/cv.contourArea(c))*50)+((cv.contourArea(cv.convexHull(c)))))
             thing = c
 
-    contours = [thing]
+            contours = [thing]
 
-    #print cv.contourArea(cv.convexHull(thing))
-    #print cv.contourArea(cv.convexHull(thing))/cv.contourArea(thing)*50
+            #print cv.contourArea(cv.convexHull(thing))
+            #print cv.contourArea(cv.convexHull(thing))/cv.contourArea(thing)*50
 
-    im4 = cv.cvtColor(im3, cv.COLOR_GRAY2BGR)
-    cv.drawContours(im4,contours,-1,(0,255,0),1)
-    cv.imwrite("filesa/"+s, im4)
-    #cv.imshow('title', im4)
-    #cv.waitKey(0)
-    #cv.destroyAllWindows()
+            im4 = cv.cvtColor(im3, cv.COLOR_GRAY2BGR)
+            #cv.imshow('color', im4)
+            cv.drawContours(im4,contours,-1,(0,255,0),1)
+            cv.imwrite("filesa/"+s, im4)
+            #cv.imshow('title', im4)
+            #cv.waitKey(0)
+            #cv.destroyAllWindows()
