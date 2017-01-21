3e7ed0bc5c4509d4807a1e39ebdd865a4641f643
Elias Basler <e.e.basler@gmail.com>
2017-01-21 18:55:48 -0500

    cv

--- CV/CV.py
+++ CV/CV.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as mpl
 
-cap = cv.VideoCapture(0)
+cap = cv.VideoCapture("greentraining.avi")
 
 #img = cv.imread('img.jpg', 0)
 
@@ -16,9 +16,8 @@ while True:
     bol, frame = cap.read()
 
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    cv.line(gray, (0,0), (100,100), (0,255,255), 20)
     cv.imshow('frame', gray)
-
+    cv.waitKey(0)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
