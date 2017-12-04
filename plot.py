8aff714fe8890071faf0cece0b24d91fdb5a890e
Elias Basler <e.e.basler@gmail.com>
2017-12-04 14:47:41 -0500

    Matplotlib

--- plot.py
+++ plot.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.text as text
 import matplotlib.pyplot as plt
 import scipy.integrate as integrate
 
@@ -15,21 +16,32 @@ def p(x):
 
 t = np.arange(0., 20.5, 0.5)
 # Create plots with pre-defined labels.
-#fig, ax = plt.subplots()
-plt.plot(t, f(t), label='Monthly Earnings')
-plt.plot(t, [p(i) for i in t], label='All Earnings')
-plt.plot(t, [62.796]*len(t), t, [5.233]*len(t), label='Break even point')
-#ax.plot(a, d, 'k:', label='Data length')
-#ax.plot(a, c + d, 'k', label='Total message length')
-plt.axis([0, 20, 0, 100])
-legend = plt.legend(loc='upper center', shadow=True, fontsize='x-large')
 
+fig = plt.figure()
+axes = fig.add_subplot(1, 1, 1)
+axes.plot(t, f(t), label='Monthly Earnings')
+axes.plot(t, [p(i) for i in t], label='All Earnings')
+axes.plot(t, [5.233]*len(t), label='Monthly Break even point')
+axes.axis([0, 20, 0, 80])
+legend = axes.legend(loc='upper left', fontsize='x-large')
+axes.set_facecolor('#795548')
+fig.set_facecolor('#795548')
 plt.xlabel('Months')
-plt.ylabel('Thousands of Dollars')
-plt.title('How we be rich')
-plt.grid(True)
-
+plt.ylabel('Dollars (in thousands)')
+axes.set_title('Income', color='#FFFFFF')
+axes.grid(True)
+plt.setp(legend.get_texts(), color='#FFFFFF')
+axes.spines['bottom'].set_color('#FFFFFF')
+axes.spines['top'].set_color('#FFFFFF')
+axes.spines['left'].set_color('#FFFFFF')
+axes.spines['right'].set_color('#FFFFFF')
+axes.xaxis.label.set_color('#FFFFFF')
+axes.tick_params(axis='x', colors='#FFFFFF')
+axes.yaxis.label.set_color('#FFFFFF')
+axes.tick_params(axis='y', colors='#FFFFFF')
 # Put a nicer background color on the legend.
-#legend.get_frame().set_facecolor('#00FFCC')
+legend.get_frame().set_facecolor('#695548')
 #print([p(i) for i in t])
-plt.show()
+fig.tight_layout()
+#plt.show()
+fig.savefig('foo.png', facecolor=fig.get_facecolor(), edgecolor=fig.get_facecolor())
